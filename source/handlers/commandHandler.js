9568b128b44560a88a593893252f7df89741f641
Lucas A <83725986+smokeeaasd@users.noreply.github.com>
2022-09-23 20:20:55 -0300

    Update commandHandler.js

--- source/handlers/commandHandler.js
+++ source/handlers/commandHandler.js
@@ -11,9 +11,9 @@ module.exports = {
         const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
         for (const file of commandFiles) {
-        const filePath = path.join(commandsPath, file);
-        const command = require(filePath);
-        client.commands.set(command.data.name, command);
+            const filePath = path.join(commandsPath, file);
+            const command = require(filePath);
+            client.commands.set(command.data.name, command);
         }
     }
 }
